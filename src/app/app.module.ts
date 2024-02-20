9ac70d21913c5cd0d802d7286fe2b00bb0233575
Timothy jan <jan.timothy@gmail.com>
2024-02-20 12:29:04 -0800

    1st version header complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,24 +9,24 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LayoutComponent } from './components/layout/layout.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { OurStoryComponent } from './components/our-story/our-story.component';
 import { TimelineComponent } from './components/timeline/timeline.component';
 import { MomentsComponent } from './components/moments/moments.component';
 import { HomeComponent } from './components/home/home.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     LayoutComponent,
-    NavBarComponent,
     OurStoryComponent,
     TimelineComponent,
     MomentsComponent,
     HomeComponent,
-    SideNavComponent
+    SideNavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
